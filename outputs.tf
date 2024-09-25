fcb042f00099dbf30c0693b87adb1a4d29249a6d
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-25 15:49:54 -0700

    vpc

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
     value = module.vpc.vpc_id
 }
 
+output "public_subnet_ids" {
+    value = module.vpc.public_subnet_ids
+}
+
 # output "aws_availability_zones" {
 #     value = module.vpc.aws_availability_zones
 # }
