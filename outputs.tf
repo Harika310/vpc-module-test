3d63b3340cad095b8ad660430284de3baba97747
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-25 15:42:15 -0700

    vpc

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 # output "aws_availability_zones" {
 #     value = module.vpc.aws_availability_zones
 # }
+
+# output "default_vpc_info" {
+#     value = module.vpc.default_vpc_info
+# }
+
+# output "main_route_table_info" {
+#     value = module.vpc.main_route_table
+# }
